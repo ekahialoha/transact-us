348f93449e3ab34ef4e0be5012e80ee3d578bdaa
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-04-25 19:43:05 -0700

    remove antd contd

--- src/components/HandleLogout.js
+++ src/components/HandleLogout.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { message } from 'antd';
 
 const HandleLogOut = props => {
   useEffect(() => {
@@ -9,7 +8,7 @@ const HandleLogOut = props => {
     } else {
       props.updateUser({});
       localStorage.removeItem('session_key');
-      message.warning('Successfully logged out');
+      // message.warning('Successfully logged out');
     }
   });
 
