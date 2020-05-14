cb9fb2293ad0ba08b6dc757ff2911c7c4e1ba5c7
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-05-14 14:14:55 -0700

    hard coded account display

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -4,26 +4,38 @@ import './style.scss';
 import AuthEnforcement from '../AuthEnforcement';
 import Api from '../Api';
 
-const makeAccounts = (regs) => regs.map((account) => {
-  account.url = `registries/${account.id}`;
-  return account;
-});
+const makeAccounts = (regs) => regs.map((account) => ({ ...account, url: `registries/${account.id}` }));
 
 const Dashboard = (props) => {
   const [accounts, setAccounts] = useState([]);
   useEffect(() => {
     Api('accounts')
       .then((result) => result.data)
-      .then((data) => setAccounts(makeAccounts(data)))
-      .catch((error) => console.log(error.response));
+      .then((data) => setAccounts(makeAccounts(data)));
+    // .catch((error) => console.log(error.response));
   }, []);
   return (
     <>
       <h1>Accounts</h1>
       <main className="em-box">
-        {accounts.map((account, key) => (
-          <div key={key}><Link to={account.url}>{account.name}</Link></div>
-        ))}
+        {/* {accounts.map((account) => (
+          <div key={account.id}><Link to={account.url}>{account.name}</Link></div>
+        ))} */}
+        <fieldset className="account-list">
+          <legend>Owned Registries</legend>
+          <section>
+            <Link to="/registries/4">Our Checking</Link>
+          </section>
+        </fieldset>
+        <fieldset className="account-list">
+          <legend>Joint Registries</legend>
+          <section>
+            <Link to="/registries/4">His Checking</Link>
+          </section>
+          <section>
+            <Link to="/registries/4">Some Awesome Company</Link>
+          </section>
+        </fieldset>
       </main>
     </>
   );
